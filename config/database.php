28107756aa0469c510078dd2e16bcc1c399159fd
Um Papi <umpapi4@gmail.com>
2026-09-25 15:02:33 +0700

    update

--- config/database.php
+++ config/database.php
@@ -5,6 +5,20 @@ use Illuminate\Support\Str;
 return [
     'default' => env('DB_CONNECTION', 'mysql'),
     'connections' => [
+        'pgsql' => [
+    'driver' => 'pgsql',
+    'url' => env('DATABASE_URL'),
+    'host' => env('DB_HOST', '127.0.0.1'),
+    'port' => env('DB_PORT', '5432'),
+    'database' => env('DB_DATABASE', 'postgres'),
+    'username' => env('DB_USERNAME', 'postgres'),
+    'password' => env('DB_PASSWORD', ''),
+    'charset' => 'utf8',
+    'prefix' => '',
+    'prefix_indexes' => true,
+    'search_path' => 'public',
+    'sslmode' => 'prefer',
+],
         'sqlite' => [
             'driver' => 'sqlite',
             'url' => env('DATABASE_URL'),
